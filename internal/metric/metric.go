d58ced77e6a4e935a998a4222b6fd5c4ddc1b125
Mert Şişmanoğlu <mert190737fb@gmail.com>
2024-10-18 23:10:18 +0300

    feat(metric): Add GetAllSystemMetrics function to retrieve all metrics in ApiResponse struct
    - The output direclty used as an API response on /metrics

---
+++ internal/metric/metric.go
@@ -0,0 +1,38 @@
+package metric
+
+type ApiResponse struct {
+	Cpu    CpuData    `json:"cpu"`
+	Memory MemoryData `json:"memory"`
+	Disk   DiskData   `json:"disk"`
+	Host   HostData   `json:"host"`
+}
+
+func GetAllSystemMetrics() (*ApiResponse, error) {
+	cpu, cpuErr := CollectCpuMetrics()
+	memory, memErr := CollectMemoryMetrics()
+	disk, diskErr := CollectDiskMetrics()
+	host, hostErr := GetHostInformation()
+
+	if cpuErr != nil {
+		return nil, cpuErr
+	}
+
+	if memErr != nil {
+		return nil, memErr
+	}
+
+	if diskErr != nil {
+		return nil, diskErr
+	}
+
+	if hostErr != nil {
+		return nil, hostErr
+	}
+
+	return &ApiResponse{
+		Cpu:    *cpu,
+		Memory: *memory,
+		Disk:   *disk,
+		Host:   *host,
+	}, nil
+}
