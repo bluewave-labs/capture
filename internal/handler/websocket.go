0c02260c4cedc169b08d63fdcd495cc44138719c
Mert Şişmanoğlu <mert190737fb@gmail.com>
2024-10-19 15:31:01 +0300

    feat(websocket): Update websocket handler to include origin check and logging

--- internal/handler/websocket.go
+++ internal/handler/websocket.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -13,11 +15,22 @@ var interval = 2 * time.Second
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// TODO: Make allowedOrigins Configurable. ENV var?, config?
+		allowedOrigins := []string{"*"}
+
+		if allowedOrigins[0] == "*" {
+			return true // Accept connections from everywhere
+		}
+
+		return false // Decline connections
+	},
 }
 
 func WebSocket(c *gin.Context) {
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
+		log.Printf("[FAIL] | Failed to set websocket upgrade: %v", err)
 		return
 	}
 
