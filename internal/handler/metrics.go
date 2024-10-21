1dd9f46072b5dae02da21f009c419c26d5bf5b12
Mert Şişmanoğlu <mert190737fb@gmail.com>
2024-10-22 02:52:18 +0300

    feat(api): Add CPU, Memory, and Disk metric specific handlers

--- internal/handler/metrics.go
+++ internal/handler/metrics.go
@@ -8,10 +8,55 @@ import (
 
 func Metrics(c *gin.Context) {
 	metrics, metricsErr := metric.GetAllSystemMetrics()
-
 	if metricsErr != nil {
-		c.Status(500)
+		c.JSON(500, "Unable to get metrics")
+		return
 	}
 
 	c.JSON(200, metrics)
+	return
+}
+
+func MetricsCPU(c *gin.Context) {
+	metrics, metricsErr := metric.CollectCpuMetrics()
+	if metricsErr != nil {
+		c.JSON(500, "Unable to get metrics")
+		return
+	}
+
+	c.JSON(200, metrics)
+	return
+}
+
+func MetricsMemory(c *gin.Context) {
+	metrics, metricsErr := metric.CollectMemoryMetrics()
+	if metricsErr != nil {
+		c.JSON(500, "Unable to get metrics")
+		return
+	}
+
+	c.JSON(200, metrics)
+	return
+}
+
+func MetricsDisk(c *gin.Context) {
+	metrics, metricsErr := metric.CollectDiskMetrics()
+	if metricsErr != nil {
+		c.JSON(500, "Unable to get metrics")
+		return
+	}
+
+	c.JSON(200, metrics)
+	return
+}
+
+func MetricsHost(c *gin.Context) {
+	metrics, metricsErr := metric.GetHostInformation()
+	if metricsErr != nil {
+		c.JSON(500, "Unable to get metrics")
+		return
+	}
+
+	c.JSON(200, metrics)
+	return
 }
