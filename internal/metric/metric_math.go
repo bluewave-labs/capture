5892db9521e0c0c3ff8815d72b93902ecdd5902a
Mert Şişmanoğlu <mert190737fb@gmail.com>
2024-10-22 02:33:41 +0300

    feat(metric): Add functions for rounding float, generating random integers and floats

---
+++ internal/metric/metric_math.go
@@ -0,0 +1,45 @@
+package metric
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"math"
+	"math/big"
+)
+
+// Round a float to a given precision and return the pointer of the result
+func RoundFloatPtr(val float64, precision uint) *float64 {
+	ratio := math.Pow(10, float64(precision))
+	prc := math.Round(val*ratio) / ratio
+	return &prc
+}
+
+func RandomIntPtr(max int64) *int {
+	n, err := rand.Int(rand.Reader, big.NewInt(max))
+	if err != nil {
+		panic(err) // handle error appropriately in production
+	}
+	result := int(n.Int64())
+	return &result
+}
+
+func RandomUInt64Ptr() *uint64 {
+	var b [8]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		panic(err) // handle error appropriately in production
+	}
+	result := binary.BigEndian.Uint64(b[:])
+	return &result
+}
+
+func RandomFloatPtr() *float64 {
+	var b [8]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		panic(err) // handle error appropriately in production
+	}
+	randomUint64 := binary.BigEndian.Uint64(b[:])
+	result := float64(randomUint64) / float64(math.MaxUint64)
+	return &result
+}
