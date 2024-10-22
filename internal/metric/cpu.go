f1bf912c5d3d86218f5b04c0f093bed14d7bccdb
Mert Şişmanoğlu <mert190737fb@gmail.com>
2024-10-22 04:11:45 +0300

    feat(metric): Update CPU metric calculations and collect usage percentage
    - Get total cpu times
    - Calculate usage and free percentage

--- internal/metric/cpu.go
+++ internal/metric/cpu.go
@@ -11,8 +11,8 @@ type CpuData struct {
 	LogicalCore  int      `json:"logical_core"`  // Logical cores aka Threads
 	Frequency    float64  `json:"frequency"`     // Frequency in mHz
 	Temperature  *float32 `json:"temperauture"`  // Temperature in Celsius (nil if not available)
-	FreePercent  *float64 `json:"free_percent"`  // TODO: Implement
-	UsagePercent *float64 `json:"usage_percent"` // TODO: Implement
+	FreePercent  float64  `json:"free_percent"`  // Free percentage  //* 1 - (Total - Idle / Total)
+	UsagePercent float64  `json:"usage_percent"` // Usage percentage //* Total - Idle / Total
 }
 
 func CollectCpuMetrics() (*CpuData, error) {
@@ -35,6 +35,17 @@ func CollectCpuMetrics() (*CpuData, error) {
 		return nil, cpuInfoErr
 	}
 
+	// Collect CPU Usage
+	cpuTimes, cpuTimesErr := cpu.Times(false)
+
+	if cpuTimesErr != nil {
+		return nil, cpuTimesErr
+	}
+
+	// Calculate CPU Usage Percentage
+	total := cpuTimes[0].User + cpuTimes[0].Nice + cpuTimes[0].System + cpuTimes[0].Idle + cpuTimes[0].Iowait + cpuTimes[0].Irq + cpuTimes[0].Softirq + cpuTimes[0].Steal + cpuTimes[0].Guest + cpuTimes[0].GuestNice
+	cpuUsagePercent := (total - (cpuTimes[0].Idle + cpuTimes[0].Iowait)) / total
+
 	// Collect CPU Temperature from sysfs
 	cpuTemp, cpuTempErr := sysfs.CpuTemperature()
 
@@ -47,7 +58,7 @@ func CollectCpuMetrics() (*CpuData, error) {
 		LogicalCore:  cpuLogicalCoreCount,
 		Frequency:    cpuInformation[0].Mhz,
 		Temperature:  cpuTemp,
-		FreePercent:  nil, // TODO: Implement
-		UsagePercent: nil, // TODO: Implement
+		FreePercent:  1 - cpuUsagePercent,
+		UsagePercent: cpuUsagePercent,
 	}, nil
 }
