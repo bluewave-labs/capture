6c99b4115fe70fd242c12cc77108740e14125b45
Alex Holliday <ajhollid@gmail.com>
2024-11-01 14:13:43 +0800

    graceful failure for memory

--- internal/metric/memory.go
+++ internal/metric/memory.go
@@ -6,10 +6,17 @@ import (
 
 func CollectMemoryMetrics() (*MemoryData, []string) {
 	var memErrors []string
+	defaultMemoryData := &MemoryData{
+		TotalBytes:     0,
+		AvailableBytes: 0,
+		UsedBytes:      0,
+		UsagePercent:   RoundFloatPtr(0, 4),
+	}
 	vMem, vMemErr := mem.VirtualMemory()
 
 	if vMemErr != nil {
 		memErrors = append(memErrors, vMemErr.Error())
+		return defaultMemoryData, memErrors
 	}
 
 	return &MemoryData{
